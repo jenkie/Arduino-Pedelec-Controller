dbe1ab32f8bf3f2b8ee4acdca95bcefd72cf2e39
Thomas Jarosch <thomas.jarosch@intra2net.com>
2014-07-22 19:04:04 +0200

    Update comment

--- Arduino_Pedelec_Controller/Arduino_Pedelec_Controller.ino
+++ Arduino_Pedelec_Controller/Arduino_Pedelec_Controller.ino
@@ -1,7 +1,7 @@
 /*
-Arduino Pedelec "Forumscontroller" for Hardware 1.1-1.5
+Arduino Pedelec "Forumscontroller" for Hardware 1.1-2.0
 written by jenkie and others / pedelecforum.de
-Copyright (C) 2012-2013
+Copyright (C) 2012-2014
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
