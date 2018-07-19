aba330adda06d957a9f683657c2a906780349141
Volker Brand <20853959+VolkerBrand@users.noreply.github.com>
2018-07-19 16:00:56 +0200

    Update config.h

--- Arduino_Pedelec_Controller/config.h
+++ Arduino_Pedelec_Controller/config.h
@@ -142,7 +142,8 @@ const int fixed_throttle_in_watts = 250;         //number of watts to set as thr
 #define BBS_GEARCHANGEPAUSE 2000 //powerless time in milliseconds to allow gear change 
 // #define SUPPORT_XCELL_RT    //uncomment if X-CELL RT connected. FC1.4: pas_factor_min=0.2, pas_factor_max=0.5. FC1.5: pas_factor_min=0.5, pas_factor_max=1.5. pas_magnets=8
 // #define SUPPORT_SEMPU_V1  //uncomment if you have a Sempu torque sensor, old type with one direction wire and one pas wire
-//#define SUPPORT_SEMPU  //uncomment if you have a Sempu torque sensor, new type with two pas wires
+// #define SUPPORT_SEMPU_T4_3A //uncomment if you have a Sempu torque sensor, of type left&right crank torque 1.5V-3V(4V), one speed signal, one direction/pedaling signal, 32 pulses per revolution
+// #define SUPPORT_SEMPU_T4_3B //uncomment if you have a Sempu torque sensor, of type left&right crank torque 1.5V-3V(4V), dual speed/direction signal cos/sin 2x16 pulses per revolution
 // #define SUPPORT_TORQUE_THROTTLE
 // #define SUPPORT_HRMI         //uncomment if polar heart-rate monitor interface connected to i2c port
 #define SUPPORT_BRAKE        //uncomment if brake switch connected
