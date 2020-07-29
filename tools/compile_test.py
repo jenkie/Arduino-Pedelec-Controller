e4489492c120ec79667523176b9bf5b5efa55bcf
Thomas Jarosch <thomas.jarosch@intra2net.com>
2020-07-29 23:38:48 +0200

    Adapt compile test to new cmake suite
    Uses /tmp as toolchain directory, which basically means
    it will detect the correct version in the homedir.

--- tools/compile_test.py
+++ tools/compile_test.py
@@ -324,7 +324,7 @@ class CompileTest(unittest.TestCase):
             os.mkdir(test_name)
         os.chdir(test_name)
 
-        subprocess.call('cmake -DDOCUMENTATION=OFF -Wno-dev ../', shell=True)
+        subprocess.call('cmake -DDOCUMENTATION=OFF -Wno-dev -DARDUINO_INSTALL_PATH=/tmp ../', shell=True)
 
     def run_make(self, build_name):
         ret = subprocess.call('make -j' + str(CPU_COUNT), shell=True)
