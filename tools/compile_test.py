a2ca3444c3268416b389104a88bccbb3f9721128
Thomas Jarosch <thomas.jarosch@intra2net.com>
2020-07-30 23:32:58 +0200

    Compile test: Add SUPPORT_THERMISTOR_CUTOFF support

--- tools/compile_test.py
+++ tools/compile_test.py
@@ -52,6 +52,7 @@ ALL_FEATURES = [
                   'SUPPORT_MOTOR_SERVO',
                   'SUPPORT_TEMP_SENSOR',
                   'SUPPORT_THERMISTOR',
+                  'SUPPORT_THERMISTOR_CUTOFF',
                   'SUPPORT_HX711',
                   'DETECT_BROKEN_SPEEDSENSOR',
                   'USE_EXTERNAL_CURRENT_SENSOR',
@@ -215,6 +216,8 @@ def write_config_h(filename=CONFIG_H,
         f.write('const float thermistor_t0=0.00335401643;     // 1/T0 of thermistor in 1/K\n')
         f.write('const float thermistor_b=0.00025316455;      // 1/beta of thermistor in 1/K\n')
         f.write('const float thermistor_r=10;                 // r of thermistor in kOhm\n')
+        f.write('const int temperature_cutoff_start=100;    //start of temperature cutoff (100% power)\n')
+        f.write('const int temperature_cutoff_stop=120;     //stop of temperature cutoff in °C (0% power)\n')
         f.write('const int pas_tolerance=1;               //0... increase to make pas sensor slower but more tolerant against speed changes\n')
         f.write('const int throttle_offset=196;           //Offset voltage of throttle control when in "0" position (0..1023 = 0..5V)\n')
         f.write('const int throttle_max=832;              //Offset voltage of throttle control when in "MAX" position (0..1023 = 0..5V)\n')
@@ -499,6 +502,7 @@ class CompileTest(unittest.TestCase):
                             'SUPPORT_GEAR_SHIFT',
                             'SUPPORT_TEMP_SENSOR',
                             'SUPPORT_THERMISTOR',
+                            'SUPPORT_THERMISTOR_CUTOFF',
                             'SUPPORT_HX711',
                             'DETECT_BROKEN_SPEEDSENSOR'
                         ]
